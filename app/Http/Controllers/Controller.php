b2ac847efde64a91809406eeb35923ffb6bccfeb
shalvah <diakon.ng@gmail.com>
2018-02-17 17:46:41 +0100

    Refactor; reformat API responses; remove unused files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected function buildFailedValidationResponse(Request $request, array $errors)
+    {
+        return new JsonResponse([
+            "status" => 'error',
+            "message" => "Validation errors",
+            "errors" => $errors
+        ], 422);
+    }
 }
---
+++ app/Services/UnnPortalScraper.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Created by shalvah
+ * Date: 17/02/2018
+ * Time: 17:37
+ */
+
+namespace App\Services;
+
+
+class UnnPortalScraper
+{
+
+    public function login($username, $password)
+    {
+        $ch = curl_init();
+        $cookieFile = "cookie.txt";
+        $userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
+        $siteUrl = "http://unnportal.unn.edu.ng/";
+        $loginUrl = "http://unnportal.unn.edu.ng/landing.aspx";
+        $profileUrl = "http://unnportal.unn.edu.ng/modules/ProfileDetails/BioData.aspx";
+
+        curl_setopt($ch, CURLOPT_URL, $siteUrl);
+        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieFile);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieFile);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $loginPage = curl_exec($ch);
+
+        $state = $this->htmlExtractValue($loginPage, "id=\"__VIEWSTATE\"");
+        $validation = $this->htmlExtractValue($loginPage, "id=\"__EVENTVALIDATION\"");
+        $postfields = array(
+            '__EVENTVALIDATION' => $validation,
+            '__EVENTARGUMENT' => "",
+            '__EVENTTARGET' => "",
+            '__VIEWSTATE' => $state,
+            'ct100' => 'on',
+            'inputUsername' => $username,
+            'inputPassword' => $password,
+            'login' => 'Login');
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_URL, $loginUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
+        $resultOfLogin = curl_exec($ch);
+
+        if ($resultOfLogin == $loginPage) {
+            return false;
+        }
+
+        curl_setOpt($ch, CURLOPT_POST, FALSE);
+        curl_setopt($ch, CURLOPT_URL, $profileUrl);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieFile);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieFile);
+        $profilePage = curl_exec($ch);
+        curl_close($ch);
+
+
+        return $profilePage;
+    }
+
+    function htmlExtractValue($htmlString, $value)
+    {
+        $str = stristr($htmlString, "$value value=\"");
+        $vals = explode('value="', $str);
+        return stristr($vals[1], '"', true);
+    }
+
+    function extractDetails()
+    {
+        $data = [];
+        $dom = new \DomDocument;
+
+        $internalErrors = libxml_use_internal_errors(true);
+
+        $dom->loadHTML($profile);
+        $dom->preserveWhiteSpace = false;
+
+        libxml_use_internal_errors($internalErrors);
+
+        $sex = $dom->getElementById("ctl00_ContentPlaceHolder1_ddlSex");
+        $sexValue = $this->domExtractSelectedValue($sex->getElementsByTagName('option'));
+
+        $dept = $dom->getElementById("ctl00_ContentPlaceHolder1_ddlDepartment");
+        $deptValue = $this->domExtractSelectedValue($dept->getElementsByTagName('option'));
+
+        $entryYear = $dom->getElementById("ctl00_ContentPlaceHolder1_ddlEntryYear");
+        $entryYearValue = $this->domExtractSelectedValue($entryYear->getElementsByTagName('option'));
+
+        $gradYear = $dom->getElementById("ctl00_ContentPlaceHolder1_ddlGradYear");
+        $gradYearValue = $this->domExtractSelectedValue($gradYear->getElementsByTagName('option'));
+
+        $level = $dom->getElementById("ctl00_ContentPlaceHolder1_ddlYearOfStudy");
+        $levelValue = $this->domExtractSelectedValue($level->getElementsByTagName('option'));
+
+        $data['surname'] = $this->htmlExtractValue($profile, "<input name=\"ctl00\$ContentPlaceHolder1\$txtSurname\" type=\"text\"");
+        $data['first_name'] = $this->htmlExtractValue($profile, "<input name=\"ctl00\$ContentPlaceHolder1\$txtFirstname\" type=\"text\"");
+        $data['middle_name'] = $this->htmlExtractValue($profile, "<input name=\"ctl00\$ContentPlaceHolder1\$txtMiddlename\" type=\"text\"");
+        $data['sex'] = $sexValue;
+        $data['mobile'] = $this->htmlExtractValue($profile, "<input name=\"ctl00\$ContentPlaceHolder1\$wmeMobileno\" type=\"text\"");
+        $data['email'] = $this->htmlExtractValue($profile, "<input name=\"ctl00\$ContentPlaceHolder1\$txtEmail\" type=\"text\"");
+        $data['department'] = $deptValue;
+        $data['level'] = $levelValue;
+        $data['entry_year'] = $entryYearValue;
+        $data['grad_year'] = $gradYearValue;
+        $data['matric_no'] = $this->htmlExtractValue($profile, "<input name=\"ctl00\$ContentPlaceHolder1\$txtMatricNo\" type=\"text\"");
+        $data['jamb_no'] = $this->htmlExtractValue($profile, "<input name=\"ctl00\$ContentPlaceHolder1\$txtJAMBNo\" type=\"text\"");
+
+        return $data;
+    }
+
+    function domExtractSelectedValue($nodeList)
+    {
+        for ($i = 0; $i < $nodeList->length; $i++) {
+            if ($nodeList->item($i)->hasAttribute('selected')
+                && $nodeList->item($i)->getAttribute('selected') === "selected"
+            ) {
+                return $nodeList->item($i)->nodeValue;
+            }
+        }
+    }
+}
